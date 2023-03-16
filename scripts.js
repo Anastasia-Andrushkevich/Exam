056ddfb29eba92b753b0f1f5c0d495e82f3b7e2f
Anastasia Andrushkevich <miss.nastya97@mail.ru>
2023-03-16 03:31:45 +0300

    delivery faq

--- scripts.js
+++ scripts.js
@@ -13,3 +13,19 @@ window.addEventListener("DOMContentLoaded", function () {
       utilsScript:
         "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
     });*/
+    window.addEventListener("DOMContentLoaded", function(){
+        const accItems = document.querySelectorAll(".accordion_item_header");
+        for(let i=0; i < accItems.length; i++){
+            accItems[i].addEventListener("click", function(event){
+                
+                [].forEach.call(accItems, function(element){
+                    element.closest(".accordion_item").classList.remove("active")
+                });
+
+                const root = event.target.closest(".accordion_item");
+                root.classList.toggle("active");
+
+            });
+        }
+    }
+    )
